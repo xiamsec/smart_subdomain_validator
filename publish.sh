799886b7f3b903baed9b98c84bd1b9472bc18403
xiamsec <siamdewan224@gmail.com>
2025-09-07 19:36:03 +0600

    xiamsec

--- publish.sh
+++ publish.sh
@@ -4,7 +4,7 @@ git init
 git config user.name "xiamsec"
 git config user.email "siamdewan224@gmail.com"
 git add -A
-git commit -m "Initial commit: smart_subdomain_validator" || true
+git commit -m "xiamsec" || true
 git branch -M main
 git remote remove origin 2>/dev/null || true
 git remote add origin https://github.com/xiamsec/smart_subdomain_validator.git
